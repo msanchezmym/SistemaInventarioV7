d6d07580acd345ad1e9cba7b255eb49e0a754a90
msanchezmym <msanchez@mym.com.pe>
2023-09-26 17:50:53 -0500

    Crear proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Nombre es requerido")]
+        [MaxLength(60,ErrorMessage ="Nombre debe ser maximo 60 caracteres")]
+        public int Nombre { get; set; }
+        
+        [Required(ErrorMessage = "Descripcion es requerido")]
+        [MaxLength(60, ErrorMessage = "Descripcion debe ser maximo 100 caracteres")]
+        public int Descripcion { get; set; }
+        
+        [Required(ErrorMessage = "Estado es requerido")]
+        public bool Estado  { get; set; }
+
+    }
+}
