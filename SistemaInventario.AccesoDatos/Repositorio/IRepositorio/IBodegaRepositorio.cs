803ea883007f57ce520086998d51d9ca16b9edb7
msanchezmym <msanchez@mym.com.pe>
2023-10-10 19:04:40 -0500

    patron repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IBodegaRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IBodegaRepositorio : IRepositorio<Bodega>
+    {
+        void Actualizar(Bodega bodega);
+
+        
+    }
+}
